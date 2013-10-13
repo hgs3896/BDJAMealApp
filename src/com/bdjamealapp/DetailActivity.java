80b01f44f118c262c792bacbd12eabe8233cec84
Ham Ji Seong <admin@brambean.com>
2013-10-13 17:31:59 +0900

    final

--- src/com/bdjamealapp/DetailActivity.java
+++ src/com/bdjamealapp/DetailActivity.java
@@ -3,7 +3,6 @@ package com.bdjamealapp;
 import android.R;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.PopupMenu;
 import android.view.MenuItem;
 import com.bdjamealapp.debug.ErrorManager;
 import com.bdjamealapp.fragment.DetailFragment;
--- src/com/bdjamealapp/NotificationActivity.java
+++ src/com/bdjamealapp/NotificationActivity.java
@@ -1,7 +1,6 @@
 package com.bdjamealapp;
 
 import android.R;
-import android.content.ContentResolver;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
